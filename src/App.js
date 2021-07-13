5846314872bf036d96d74f8fc411b5f3ecaa28a2
Peter <peter.gosling@landor.com>
2021-07-13 15:18:18 +0100

    Weather App Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Forecast from "./components/Forecast/Forecast";
+import Logo from './components/Logo/Logo';
+
+function App() {
+ return (
+   <div className="App">
+     <header className="App-header">
+       <Logo />
+       <h1>Peters React Weather App</h1>
+     </header>
+     <main>
+       <Forecast />
+     </main>
+     <footer>
+       Page created by Peter Gosling
+     </footer>
+   </div>
+ );
+}
+
+export default App;
